12b1027b9e10368b16353c2d47256ad34078fa1e
Jyri Andrejev <jyriand@gmail.com>
2013-07-10 19:11:59 +0300

    Read html from index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+
+  var index = fs.readFileSync('index.html', 'utf-8');
+
+  response.send(index);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
