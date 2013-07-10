1544c72daacae10ede56ef526fcd6bc2ae2efaf5
Ubuntu <ubuntu@ip-10-147-139-203.ec2.internal>
2013-07-10 17:51:36 +0000

    Code from assignment 3

--- web.js
+++ web.js
@@ -10,7 +10,7 @@ app.get('/', function(request, response) {
   response.send(index);
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
